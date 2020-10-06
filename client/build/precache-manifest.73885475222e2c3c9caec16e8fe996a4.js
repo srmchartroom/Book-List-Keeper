22dcc36019555ecbbaf7cf07799dbe4db3912ece
srmchartroom <sean@chartroomcreative.com>
2020-10-06 11:06:06 -0400

    post-build deploy

---
+++ client/build/precache-manifest.73885475222e2c3c9caec16e8fe996a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1dfcc6b5da3efe8bd375facea80765",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac5bd6de7669ae942f6c",
+    "url": "/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "a772f5646685c85634d6",
+    "url": "/static/js/2.bdc43e03.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/static/js/2.bdc43e03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac5bd6de7669ae942f6c",
+    "url": "/static/js/main.c738a057.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
